8ebdb6345229bf6b4bf678f31cb882e1f9c28a22
Nayak <itsmedupesh@gmail.com>
2025-11-19 16:43:04 +0530

    fixed viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || './',
   plugins: [react()],
   resolve: {
     alias: {
